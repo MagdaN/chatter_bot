9c0cb2ab90c52dd00ddad98ff4af81299342d8f2
Jochen Klar <mail@jochenklar.de>
2019-08-23 16:39:52 +0200

    Configure logging

--- chat/models.py
+++ chat/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
+from django.utils.translation import gettext_lazy as _
 
 from django_extensions.db.models import TimeStampedModel
 
--- config/settings.py
+++ config/settings.py
@@ -133,3 +133,69 @@ CHATTERBOT = {
     ],
     'storage_adapter': 'chatterbot.storage.DjangoStorageAdapter'
 }
+
+LOGGING_DIR = os.getenv('DJANGO_LOGGING_DIR', 'log')
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        },
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue'
+        }
+    },
+    'formatters': {
+        'default': {
+            'format': '[%(asctime)s] %(levelname)s: %(message)s'
+        },
+        'name': {
+            'format': '[%(asctime)s] %(levelname)s %(name)s: %(message)s'
+        },
+        'console': {
+            'format': '[%(asctime)s] %(message)s'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'error_log': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(LOGGING_DIR, 'error.log'),
+            'formatter': 'default'
+        },
+        'chat_log': {
+            'level': 'DEBUG',
+            'class':'logging.FileHandler',
+            'filename': os.path.join(LOGGING_DIR, 'chat.log'),
+            'formatter': 'name'
+        },
+        'console': {
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'console'
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+        'django.request': {
+            'handlers': ['mail_admins', 'error_log'],
+            'level': 'ERROR',
+            'propagate': True
+        },
+        'chat': {
+            'handlers': ['chat_log'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+            'propagate': False
+        }
+    }
+}
