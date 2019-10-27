1b64c676c1154fddc8d4a597c5137f7b7a145ebd
Magdalena Noffke <m.noffke@liqd.net>
2019-10-27 20:34:10 +0100

    Make serializer aware of sended question

--- chat/serializers.py
+++ chat/serializers.py
@@ -3,4 +3,5 @@ from rest_framework import serializers
 
 class ChatbotSerializer(serializers.Serializer):
 
+    in_response_to = serializers.CharField(required=True)
     text = serializers.CharField(required=True)
--- chat/viewsets.py
+++ chat/viewsets.py
@@ -21,6 +21,14 @@ class ChatbotViewSet(GenericViewSet):
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-
-        response = self.chatterbot.get_response(serializer.data)
+        text = serializer.data['text']
+        additional = {
+            'in_response_to': serializer.data['in_response_to']
+        }
+        
+        response = self.chatterbot.get_response(
+            text, 
+            additional_response_selection_parameters=additional
+            )        
+        
         return Response(response.serialize(), status=200)
