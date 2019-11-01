68f3f9f3c0f32a61873c6dfa9ac2ed81ef3cca7f
Jochen Klar <mail@jochenklar.de>
2019-11-01 12:52:11 +0100

    Fix serializer

--- chat/serializers.py
+++ chat/serializers.py
@@ -3,5 +3,4 @@ from rest_framework import serializers
 
 class ChatbotSerializer(serializers.Serializer):
 
-    previous = serializers.IntegerField(allow_null=True, required=False)
     text = serializers.CharField(required=True)
