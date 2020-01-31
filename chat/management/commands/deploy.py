a2671bf482ff1cfa42e137efa6b761a2e376ca40
Jochen Klar <mail@jochenklar.de>
2020-01-31 10:45:48 +0100

    Add deploy management script

---
+++ chat/management/commands/deploy.py
@@ -0,0 +1,11 @@
+import subprocess
+
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        subprocess.call(['/bin/bash', '-i', '-c', 'npm install; npm run build:prod'])
+        call_command('collectstatic', '--noinput')
